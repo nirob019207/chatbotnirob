db31860c0b60481f7901c5cc17659c3efedfadc8
kamran hasan <kamranhasan01920708568@gmail.com>
2023-08-08 20:20:16 +0600

    my first

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/posts',  // Proxy requests to JSONPlaceholder
+    createProxyMiddleware({
+      target: 'http://104.198.53.36:1980/filestorage/upload',
+      changeOrigin: true,
+    })
+  );
+};
